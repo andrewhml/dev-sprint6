619a9ef13fc0c82bc849265f10b0f57b7fa65a6a
Andrew Lee <ALee@Andrews-MacBook-Pro-6.local>
2014-02-27 19:55:14 -0500

    Chapter 14 problem

--- chap14.py
+++ chap14.py
@@ -1,1 +1,11 @@
 # This is where chapter 14 exercises go.
+
+def sed(patstr, repstr, file1, file2):
+	fin1 = open(file1)
+	try:
+		fin2 = open(file2)
+	except IOError:
+		print 'IOError'
+
+
+sed('hi', 'hello', 'filetext.txt', 'bad_file.txt')
