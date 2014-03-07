ef70fddce8685f4b7c91dbe960e2cd90569da9a0
Andrew Lee <ALee@Andrews-MacBook-Pro-6.local>
2014-03-06 21:36:00 -0500

    Question 14 partially finished

--- chap14.py
+++ chap14.py
@@ -1,5 +1,9 @@
 # This is where chapter 14 exercises go.
 
+import urllib
+
+# 14.2
+
 def sed(patstr, repstr, file1, file2):
 	fin1 = open(file1)
 	fin2 = open(file2, 'w')
@@ -8,5 +12,27 @@ def sed(patstr, repstr, file1, file2):
 			fin2.write(repstr+'\n')
 		else:
 			fin2.write(line)
+	fin1.close()
+	fin2.close()
 
-sed('hi', 'hello', 'first_file.txt', 'second_file.txt')
+#sed('hi', 'hello', 'first_file.txt', 'second_file.txt')
+
+
+# 14.6
+#Build the url you will need to use to get information
+def buildurl():
+	zipcode = raw_input('Enter Zipcode --> ')
+	compurl = 'http://www.uszip.com/zip/' + zipcode
+	return compurl
+
+def zipinfo():
+	myurl = buildurl()
+	conn = urllib.urlopen(myurl)
+	for line in conn:
+		stripline = line.strip()
+		title = stripline.find('<title>')
+		if title == 0:
+			end = stripline.find(',')
+			print stripline[7:end]
+			
+zipinfo()
